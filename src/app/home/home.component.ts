9924f800371afc1acb9dddc2f4c82f3b65966b09
Max Dell-Thibodeau <maxdell-thibodeau@hotmail.com>
2021-03-25 18:13:16 -0400

    angular files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  //[x: string]:this.data.username;
+
+  userForm: FormGroup;
+  listData: any;
+
+  constructor(private fb:FormBuilder) {
+    this.listData=[];
+    this.userForm = this.fb.group ({
+      name :['', Validators.required],
+      phoneNumber: ['', Validators.required]
+    })
+   }
+
+   public addItem() : void {
+    this.listData.push(this.userForm.value);
+    this.userForm.reset();
+   }
+
+   reset() {
+     this.userForm.reset();
+   }
+
+   removeItem(element: any) {
+     this.listData.forEach((value: any, index: any) => {
+       if (value == element)
+         this.listData.splice(index, 1);
+     });
+   }
+
+  ngOnInit() {
+  }
+
+}
